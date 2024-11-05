972551eb09a41e503ffb3aebedd3df705aba4703
Nastya <anastasion0107@gmail.com>
2024-11-05 12:45:03 +0700

    create migrate. add new books, authors, genre

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book
+
+all_books = Book.objects.all()
+wild_books = Book.objects.filter(title__contains='wild')
+number_wild_books = Book.objects.filter(title__contains='wild').count()
+books_containing_genre = Book.objects.filter(genre__name__icontains='fiction')

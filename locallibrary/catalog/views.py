18594bb6bff070a55983d65865d42d62203fa461
Nastya <anastasion0107@gmail.com>
2024-11-06 00:09:27 +0700

    init templates

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Book
+from .models import Book, BookInstance, Author
 
 all_books = Book.objects.all()
 wild_books = Book.objects.filter(title__contains='wild')
 number_wild_books = Book.objects.filter(title__contains='wild').count()
 books_containing_genre = Book.objects.filter(genre__name__icontains='fiction')
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors=Author.objects.count()
+
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books': num_books,
+            'num_instances': num_instances,
+            'num_instances_available': num_instances_available,
+            'num_authors': num_authors
+        }
+    )

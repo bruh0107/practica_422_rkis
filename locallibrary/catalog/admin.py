a240244d1ae1b9fd93450543ac993192103539c3
Nastya <anastasion0107@gmail.com>
2024-11-05 23:21:32 +0700

    finished admin panel

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,5 @@
+from dataclasses import fields
+
 from django.contrib import admin
 from .models import Author, Genre, Book, BookInstance, Language
 
@@ -10,15 +12,32 @@ from .models import Author, Genre, Book, BookInstance, Language
 admin.site.register(Genre)
 admin.site.register(Language)
 
+class BooksInline(admin.TabularInline):
+    model = Book
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+@admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ("last_name", "first_name", "date_of_birth", "date_of_death")
-
-admin.site.register(Author, AuthorAdmin)
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
 
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        })
+    )

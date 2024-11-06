82099c136cf4c9e52c1af6990afff37d7cfc1db5
Nastya <anastasion0107@gmail.com>
2024-11-06 09:40:12 +0700

    init templates modified function to display books that contain a word in their titles

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,7 +1,9 @@
+from re import search
+
 from django.shortcuts import render
 
 # Create your views here.
-from .models import Book, BookInstance, Author
+from .models import Book, BookInstance, Author, Genre, Language
 
 all_books = Book.objects.all()
 wild_books = Book.objects.filter(title__contains='wild')
@@ -13,14 +15,18 @@ def index(request):
     num_instances = BookInstance.objects.all().count()
     num_instances_available = BookInstance.objects.filter(status__exact='a').count()
     num_authors=Author.objects.count()
+    num_genres = Genre.objects.count()
+    num_books_with_word = Book.objects.filter(title__icontains=search).count()
 
     return render(
         request,
-        'main/index.html',
+        'index.html',
         context={
             'num_books': num_books,
             'num_instances': num_instances,
             'num_instances_available': num_instances_available,
-            'num_authors': num_authors
+            'num_authors': num_authors,
+            'num_genres': num_genres,
+            'num_books_with_word': num_books_with_word
         }
     )
